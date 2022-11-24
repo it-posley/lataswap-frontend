cfd3468c3a9e56287dcdab6dd52c6b266903965f
posley <it@pos-ley.com>
2022-11-24 11:11:43 +0800

    ci: wrap app with WagmiConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,36 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import {
+  createClient,
+  configureChains,
+  defaultChains,
+  WagmiConfig,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 
 import "../styles/globals.css";
+
+const { provider, webSocketProvider } = configureChains(defaultChains, [
+  publicProvider(),
+]);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <WagmiConfig client={client}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </WagmiConfig>
   );
 };
 
